a142cdfe336d8c1d42a4a936c81f49356a3d94a0
Laurie McLaughlin <lnmclaughlin@gmail.com>
2023-04-28 13:02:15 -0400

    created product context and product card

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import { UserContext } from "./context/UserContext";
 import Home from "./routes/home/Home";
 import Navigation from "./routes/navigation/Navigation";
 import Authentication from "./routes/authentication/Authentication";
-
-const Shop = () => {
-  return <h1>Shop Page</h1>;
-};
+import Shop from "./routes/shop/Shop";
 
 const App = () => {
   const { currentUser } = useContext(UserContext);
--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import { UserProvider } from "./context/UserContext";
+import { ProductsProvider } from "./context/ProductsContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
