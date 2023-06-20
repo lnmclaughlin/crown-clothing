f6734c205d50f92d9d43417c70d9021815e32712
Laurie McLaughlin <lnmclaughlin@gmail.com>
2023-06-20 17:23:42 -0400

    checkout component

---
+++ src/components/checkout-item/CheckoutItem.js
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
+import "./checkout-item.styles.scss";
+
+const CheckoutItem = () => {
+  return <div className="checkout-item-container"></div>;
+};
+
+export default CheckoutItem;
